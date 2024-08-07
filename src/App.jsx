14d5b0851c263e07ed058502219785184ec41b65
AbisheikChandraSekaran <abisheik.c2022cse@sece.ac.in>
2024-08-07 11:27:00 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
-import Header from './components/Header'
-import Products from './components/Products'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeLayout from "./components/HomeLayout";
 
 const App = () => {
-  const[cart,setCart] = useState([]);
   return (
     <Provider store={store}>
-        <Header cartLength={cart.length}/>
-        <Products setCart={setCart}/>
-        </Provider>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeLayout />}>
+            <Route path="/" element={<Products />} />
+            <Route path="/cart" element={<h2>Cart Page</h2>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
